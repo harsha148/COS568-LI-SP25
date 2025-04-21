0f0b9733d4f81159a17cff047db0d97b94e215ba
harsha148 <harsha.kantamneni148@gmail.com>
2025-04-21 01:31:34 -0400

    error fix benchmakr

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -7,5 +7,3 @@ void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark) {
   // Only run with default parameters
   benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<0>, 64>>();
 }
-
-INSTANTIATE_TEMPLATES_MULTITHREAD(benchmark_64_hybrid_pgm_lipp, uint64_t);
