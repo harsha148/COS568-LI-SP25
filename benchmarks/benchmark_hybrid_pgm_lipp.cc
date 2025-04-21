50dc57573f572fbc60a71ef91641130778cb1e62
harsha148 <harsha.kantamneni148@gmail.com>
2025-04-21 03:35:13 -0400

    Updated default settings

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -5,5 +5,5 @@
 
 void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark) {
   // Only run with default parameters
-  benchmark.template Run<HybridPGMLIPP<uint64_t, BranchingBinarySearch<64>, 64>>();
+  benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<uint64_t>, 64>>();
 }
